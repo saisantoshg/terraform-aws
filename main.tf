c71f1bc1da7efbd844fb36a4f63b8bb78f65bc95
saisantoshg <49271883+saisantoshg@users.noreply.github.com>
2020-04-27 22:08:24 +0530

    1

--- main.tf
+++ main.tf
@@ -6,3 +6,27 @@ provider "aws" {
 resource "aws_iam_user" "iam_user"{
   name="sai"
   }
+
+resource "aws_iam_policy" "s3_user_policy"{
+  name="s3_fullaccess_policy"
+  description="give sai s3 full access"
+  
+  policy=<<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": "*"
+        }
+    ]
+  }
+  EOF
+  }
+resource "aws_iam_policy_attachment" "iam_attach_policy"{
+  name       = "test-attachment"
+  users      = ["${aws_iam_user.iam_user.name}"]
+  policy_arn = "${aws_iam_policy.s3_user_policy.arn}"
+  
+  }
