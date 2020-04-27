e7924286d78b77b8e7b4ea4fbe337c9bb018a31b
saisantoshg <49271883+saisantoshg@users.noreply.github.com>
2020-04-27 15:04:10 +0530

    first ec2

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
+provider "aws"{
+  region="ap-south-1"
+  }
 
+resource "aws ec2 instance" "redhat"{
+    ami="ami-0a74bfeb190bd404f"
+    instance_type="t2.micro"
+    availability_zone="ap-south-1"
+  }
